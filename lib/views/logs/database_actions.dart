527ce1517136da901ef7711fdc16d83efc940ff2
Baidyanath Kundu <kundubaidya99@gmail.com>
2020-11-29 03:40:09 +0530

    Change logs ordering

--- lib/views/logs/database_actions.dart
+++ lib/views/logs/database_actions.dart
@@ -4,7 +4,7 @@ import 'package:folio/models/database/trade_log.dart';
 class DatabaseActions {
   static Future<List<TradeLog>> getAllLogs() async {
     List<Map> tuples = await Db()
-        .getOrdered(Db.tblTradeLog, '${Db.colDate} ASC, ${Db.colCode} ASC');
+        .getOrdered(Db.tblTradeLog, '${Db.colDate} DESC, ${Db.colCode} ASC');
 
     List<TradeLog> logs = [];
     tuples.forEach((row) => logs.add(TradeLog.fromDbTuple(row)));
